9c258bf24b3fd19457489e92d96a8673df808a64
Pedro Folego <hmfpedro@hotmail.com>
2022-07-29 15:53:04 -0300

    feat: CarModel

--- src/intefaces/ICar.ts
+++ src/intefaces/ICar.ts
@@ -1,8 +1,9 @@
 import { z } from 'zod';
+import { IVehicle } from './IVehicle';
 
 const carSchema = z.object({
   doorsQty: z.number().min(2).max(4),
   seatsQty: z.number().min(2).max(7),
 });
 
-export type ICar = z.infer<typeof carSchema>;
+export type ICar = IVehicle & z.infer<typeof carSchema>;
