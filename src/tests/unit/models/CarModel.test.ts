23626f676fcad99f6d2f0fea88f487328014ae5a
Pedro Folego <hmfpedro@hotmail.com>
2022-07-29 18:06:04 -0300

    test: mongoModel

--- src/tests/unit/models/CarModel.test.ts
+++ src/tests/unit/models/CarModel.test.ts
@@ -31,32 +31,53 @@ describe('Car Model', () => {
   })
   describe('Creatiing Car', () => {
     it('Successfully created', async () => {
-      const newCar = carModel.create(carMock);
+      const newCar = await carModel.create(carMock);
       expect(newCar).to.be.eql(carMock);
     });
   })
   describe('Finding Cars', () => {
     it('Successfully finded', async () => {
-      const cars = carModel.read();
+      const cars = await carModel.read();
       expect(cars).to.be.eql(carsMock);
     });
   })
   describe('Finding One Car', () => {
     it('Successfully finded', async () => {
-      const car = carModel.readOne('4edd40c86762e0fb12000003');
+      const car = await carModel.readOne('4edd40c86762e0fb12000003');
       expect(car).to.be.eql(carMockWithId);
     });
+    it('_id not found', async () => {
+      try {
+      await carModel.readOne('INVALIDID123');
+      } catch (error: any) {
+        expect(error.message).to.be.eq('InvalidMongoId')
+      }
+    })
   })
   describe('Updating One Car', () => {
     it('Successfully updated', async () => {
-      const car = carModel.update('4edd40c86762e0fb12000003', carMock);
+      const car = await carModel.update('4edd40c86762e0fb12000003', carMock);
       expect(car).to.be.eql(carMockWithId);
     });
+    it('_id not found', async () => {
+      try {
+      await carModel.readOne('INVALIDID123');
+      } catch (error: any) {
+        expect(error.message).to.be.eq('InvalidMongoId')
+      }
+    })
   })
   describe('Deleting Car', () => {
     it('Successfully deleted', async () => {
-      const car = carModel.delete('4edd40c86762e0fb12000003');
+      const car = await carModel.delete('4edd40c86762e0fb12000003');
       expect(car).to.be.eql(carMockWithId);
     });
+    it('_id not found', async () => {
+      try {
+      await carModel.readOne('INVALIDID123');
+      } catch (error: any) {
+        expect(error.message).to.be.eq('InvalidMongoId')
+      }
+    })
   })
 });
